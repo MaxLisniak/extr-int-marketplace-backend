01e2e9216291ff6bddf91421f826b961b49c3f35
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-08-20 12:28:40 +0300

    subcategories-api update

--- routes/subcategories.js
+++ routes/subcategories.js
@@ -2,6 +2,7 @@ var express = require('express');
 const deleteController = require('../controllers/deleteController');
 const patchController = require('../controllers/patchController');
 const postController = require('../controllers/postController');
+const Category = require('../models/Categoty');
 const Subcategory = require('../models/Subcategory');
 var router = express.Router();
 
@@ -25,6 +26,21 @@ router.get('/:id', async (req, res, next) => {
         .findById(req.params.id)
         .withGraphFetched("category")
     return res.send(subcategory);
+  } catch (err) {
+    console.log(err);
+    // Internal Server Error
+    return res.sendStatus(500);
+  }
+});
+router.get('/by-category-id/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const subcategories =
+      await Category
+        .relatedQuery('subcategories')
+        .for(id)
+    // .withGraphFetched("category")
+    return res.send(subcategories);
   } catch (err) {
     console.log(err);
     // Internal Server Error
