0f126eed814f990bffd35b27505ab7f43e6ceee3
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-08-27 22:51:52 +0300

    fetch products change

--- routes/products.js
+++ routes/products.js
@@ -43,26 +43,22 @@ router.get('/full/', async (req, res, next) => {
   }
 });
 
-router.get('/brief', async (req, res, next) => {
+router.get('/explore', async (req, res, next) => {
   try {
     const {
-      priceOrder,
+      // priceOrder,
       selectedCategoryName,
       selectedSubcategoryName,
-      minPrice,
-      maxPrice,
+      // minPrice,
+      // maxPrice,
     } = req.query;
 
-    let { selectedCharacteristics } = req.query;
-    selectedCharacteristics = JSON.parse(selectedCharacteristics);
-
-    const category = await Category.query()
-      .where("name", selectedCategoryName);
-
-    if (!category[0])
+    if (!selectedCategoryName || !selectedSubcategoryName) {
       return res.sendStatus(400);
+    }
 
-
+    let { selectedCharacteristics } = req.query;
+    // selectedCharacteristics = JSON.parse(selectedCharacteristics);
 
     const allProducts = await
       Product.query()
@@ -90,60 +86,39 @@ router.get('/brief', async (req, res, next) => {
           'subcategories.category_id',
           'categories.id'
         )
-        .skipUndefined()
         .where("categories.name", selectedCategoryName)
-        .skipUndefined()
         .where("subcategories.name", selectedSubcategoryName)
-        .havingBetween('latest_price', [minPrice, maxPrice])
-        // .innerJoin(
-        //   "characteristics",
-        //   "characteristics.product_id",
-        //   "products.id"
-        // )
-        .orderBy('latest_price', priceOrder);
+    // .havingBetween('latest_price', [minPrice, maxPrice])
 
-    const filtered = allProducts.filter(product => {
-      let filter = true;
-      // console.log("Considering " + product.name)
-      for (const item of Object.entries(selectedCharacteristics)) {
-        let [characteristic_name_id, values] = item;
-        if (values.length === 0) continue;
-        characteristic_name_id = Number(characteristic_name_id);
-        // console.log("\ttesting for " + characteristic_name_id + " characteristic")
-        for (const productChar of product.characteristics) {
-          // console.log("checking characteristic " + productChar.characteristic_name_id)
-          if (productChar.characteristic_name_id === characteristic_name_id) {
-            // console.log("match!")
-            // console.log(values)
-            if (!values.includes(productChar.value)) {
-              filter = false
-            }
-            break;
-          }
-        }
-        if (!filter) break;
-      }
+    // .orderBy('latest_price', priceOrder);
 
-      return filter;
-      //make verdict for this product
-    })
+    // const filtered = allProducts.filter(product => {
+    //   let filter = true;
+    //   // console.log("Considering " + product.name)
+    //   for (const item of Object.entries(selectedCharacteristics)) {
+    //     let [characteristic_name_id, values] = item;
+    //     if (values.length === 0) continue;
+    //     characteristic_name_id = Number(characteristic_name_id);
+    //     // console.log("\ttesting for " + characteristic_name_id + " characteristic")
+    //     for (const productChar of product.characteristics) {
+    //       // console.log("checking characteristic " + productChar.characteristic_name_id)
+    //       if (productChar.characteristic_name_id === characteristic_name_id) {
+    //         // console.log("match!")
+    //         // console.log(values)
+    //         if (!values.includes(productChar.value)) {
+    //           filter = false
+    //         }
+    //         break;
+    //       }
+    //     }
+    //     if (!filter) break;
+    //   }
 
-    // subquery += `where characteristics.characteristic_name_id = ${characteristic_name_id}\n`;
-    // values.map(value => {
-    //   subquery += `\tand characteristics.value = "${value}"\n`
+    //   return filter;
+    //   //make verdict for this product
     // })
 
-
-    // console.log(selectedCharacteristics)
-    // console.log("all products")
-    // console.log(allProducts.map(p => p.name))
-    // console.log("filtered")
-    // console.log(filtered.map(p => p.name))
-
-    // allProducts.map(prod => {
-    //   console.log(prod.characteristics)
-    // })
-    return res.send(filtered);
+    return res.send(allProducts);
   } catch (err) {
     console.log(err);
     // Internal Server Error
