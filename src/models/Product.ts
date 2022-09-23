008a1b157a533b6396885fc809131618c2fa080f
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-09-23 17:07:25 +0300

    remove favorites, prices, subcategories

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,11 +1,8 @@
 import Model from "./BaseModel";
 import CharacteristicValue from "./CharacteristicValue";
-import Characteristic from "./CharacteristicValue";
 import Comment from "./Comment";
 import Favorite from "./Favorite";
 import Keyword from "./Keyword";
-import Price from "./Price";
-import Subcategory from "./Subcategory";
 
 class Product extends Model {
   id: number
@@ -13,7 +10,6 @@ class Product extends Model {
   description: string
   image_url: string
   category_id: number
-  // number_of_favorites: number
   price: number
 
   static get tableName() {
@@ -21,28 +17,12 @@ class Product extends Model {
   }
 
   static relationMappings = {
-    subcategory: {
-      relation: Model.BelongsToOneRelation,
-      modelClass: Subcategory,
-      join: {
-        from: 'subcategories.id',
-        to: 'products.subcategory_id'
-      }
-    },
     comments: {
       relation: Model.HasManyRelation,
       modelClass: Comment,
       join: {
         from: "products.id",
         to: "comments.product_id"
-      }
-    },
-    prices: {
-      relation: Model.HasManyRelation,
-      modelClass: Price,
-      join: {
-        from: "products.id",
-        to: "prices.product_id"
       }
     },
     favorites: {
