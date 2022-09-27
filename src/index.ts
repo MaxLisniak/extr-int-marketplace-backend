3a66b5f7c4efe6fa32fb053617d21f4829e19ddf
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-09-27 11:37:45 +0300

    fix index.js

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,14 @@
-import logger from "./logger";
-
-/**
- * Module dependencies.
- */
-
-import app from './app';
 import Debug from "debug";
 import http from 'http';
 const debug = Debug('express:server');
-
-/**
- * Get port from environment and store in Express.
- */
+import app from './app';
 
 const port = parseInt(process.env.PORT);
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen(port);
-server.on('error', onError);
 server.on('listening', onListening);
-
-
-function onError(error: any) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      logger.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      logger.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
   const addr = server.address();
