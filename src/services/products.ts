fa8b5541f88aa839f69e9cb353ead96e0726016a
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-09-30 12:08:23 +0300

    correct fetch of products by category

--- src/services/products.ts
+++ src/services/products.ts
@@ -6,13 +6,20 @@ import {
 } from "../validationSchemas/product"
 import Product from "../models/Product"
 import CharacteristicValue from "../models/CharacteristicValue"
+import Category from "../models/Categoty"
 const PRODUCTS_PER_PAGE = 1
 
-export function findProducts(params: productFindPayloadType) {
+export async function findProducts(params: productFindPayloadType) {
   const query = Product.query()
-  // fetch products that belong to a particular category
+  // fetch products that belong to a particular category if it's a tree leaf
   if (params.category_id) {
-    query.where('category_id', params.category_id)
+    const childrenCategories = await Category
+      .query()
+      .where('parent_id', params.category_id);
+    console.log(childrenCategories)
+    if (childrenCategories.length === 0) {
+      query.where('category_id', params.category_id)
+    } else throw new Error("The specified category cannot be used for selecting products")
   }
   // limit products to those which name is like specified
   if (params.search_query) {
