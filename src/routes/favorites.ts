de5c500a3fe85354c075713dae14476603e24efb
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-10-17 17:27:30 +0300

    favorites route updates

--- src/routes/favorites.ts
+++ src/routes/favorites.ts
@@ -2,8 +2,8 @@ import Router from "express-promise-router";
 import { addFavoriteProductController, findFavoriteProductsController, removeFavoriteProductController } from "../controllers/favorites";
 const router = Router();
 
-router.post('/add', addFavoriteProductController)
-router.post('/remove', removeFavoriteProductController)
+router.post('/', addFavoriteProductController)
+router.delete('/', removeFavoriteProductController)
 router.get('/:user_id', findFavoriteProductsController)
 
 export default router;
