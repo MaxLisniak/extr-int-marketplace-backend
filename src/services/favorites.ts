502a1d90c2a466746fc36225f6b15c5f4af5055c
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-10-31 14:43:15 +0200

    favorites service imports update

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -1,6 +1,6 @@
 import User from "../models/User"
 import Favorite from "../models/Favorite"
-import { addFavoritePayloadType, favoriteFindPayloadType, removeFavoritePayloadType } from "../validationSchemas/favorite"
+import { addFavoritePayloadType, favoriteProductsFindPayloadType, removeFavoritePayloadType } from "../validationSchemas/favorite"
 
 export async function addFavoriteProduct(payload: addFavoritePayloadType) {
 
@@ -21,7 +21,7 @@ export async function removeFavoriteProduct(payload: removeFavoritePayloadType) 
     .where('products.id', product_id)
 }
 
-export function findFavoriteProducts(params: favoriteFindPayloadType) {
+export function findFavoriteProducts(params: favoriteProductsFindPayloadType) {
   return Favorite
     .query()
     .where({ user_id: params.user_id })
