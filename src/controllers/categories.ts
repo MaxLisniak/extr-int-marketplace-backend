1cd3a8dec8d5eeaf89c6c643953cf8ebaf7a9032
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-10-31 14:34:49 +0200

    category validation improvement

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -3,8 +3,8 @@ import {
   categoryCreatePayloadSchema,
   categoryUpdatePayloadSchema,
   categoryFindOnePayloadSchema,
+  categoryDeletePayloadSchema,
 } from "../validationSchemas/category";
-import { idSchema } from '../validationSchemas/id';
 import {
   findCategories,
   findCategoryById,
@@ -37,21 +37,22 @@ export async function findCategoryByIdController(req: Request, res: Response): P
 }
 
 export async function createCategoryController(req: Request, res: Response): Promise<void> {
-  const payload = categoryCreatePayloadSchema
-    .validateSync(req.body, { stripUnknown: true })
+  const payload = await categoryCreatePayloadSchema
+    .validate(req.body, { stripUnknown: true })
   const category = await createCategory(payload)
   res.json({ data: category });
 }
 
 export async function updateCategoryController(req: Request, res: Response): Promise<void> {
-  const payload = categoryUpdatePayloadSchema
-    .validateSync({ ...req.body, ...req.params }, { stripUnknown: true })
+  const payload = await categoryUpdatePayloadSchema
+    .validate({ ...req.body, ...req.params }, { stripUnknown: true })
   const category = await updateCategory(payload.id, payload)
   res.json({ data: category });
 }
 
 export async function deleteCategoryController(req: Request, res: Response): Promise<void> {
-  const payload = idSchema.validateSync(req.params, { stripUnknown: true })
+  const payload = await categoryDeletePayloadSchema
+    .validate(req.params, { stripUnknown: true })
   await deleteCategory(payload.id)
   res.sendStatus(200);
 }
--- src/validationSchemas/category.ts
+++ src/validationSchemas/category.ts
@@ -1,4 +1,14 @@
+import Category from '../models/Categoty';
 import * as yup from 'yup';
+
+
+export const categoryFindOnePayloadSchema = yup.object().shape({
+  id: yup
+    .number()
+    .integer()
+    .positive()
+    .required(),
+})
 
 export const categoryCreatePayloadSchema = yup.object().shape({
   name: yup
@@ -10,6 +20,11 @@ export const categoryCreatePayloadSchema = yup.object().shape({
     .number()
     .integer()
     .positive()
+    .test(
+      'categoryCreate-parentCategoryDoesNotExist',
+      "Can't create category, parent category does not exist",
+      async value => !value || Boolean(await Category.query().findById(value))
+    ),
 });
 
 export const categoryUpdatePayloadSchema = yup.object().shape({
@@ -17,7 +32,12 @@ export const categoryUpdatePayloadSchema = yup.object().shape({
     .number()
     .integer()
     .positive()
-    .required(),
+    .required()
+    .test(
+      'categoryUpdate-entryDoesNotExist',
+      "Can't update category, it does not exist",
+      async value => Boolean(await Category.query().findById(value))
+    ),
   name: yup
     .string()
     .min(1)
@@ -26,17 +46,28 @@ export const categoryUpdatePayloadSchema = yup.object().shape({
     .number()
     .integer()
     .positive()
+    .test(
+      'categoryUpdate-parentCategoryDoesNotExist',
+      "Can't update category, parent category does not exist",
+      async value => !value || Boolean(await Category.query().findById(value))
+    ),
 });
 
-export const categoryFindOnePayloadSchema = yup.object().shape({
+export const categoryDeletePayloadSchema = yup.object().shape({
   id: yup
     .number()
     .integer()
     .positive()
-    .required(),
+    .required()
+    .test(
+      'categoryDelete-entryDoesNotExist',
+      "Can't delete category, it does not exist",
+      async value => Boolean(await Category.query().findById(value))
+    )
 })
 
 
 export type categoryFindOnePayloadType = yup.InferType<typeof categoryFindOnePayloadSchema>
 export type categoryCreatePayloadType = yup.InferType<typeof categoryCreatePayloadSchema>
 export type categoryUpdatePayloadType = yup.InferType<typeof categoryUpdatePayloadSchema>
+export type categoryDeletePayloadType = yup.InferType<typeof categoryDeletePayloadSchema>
